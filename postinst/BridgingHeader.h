bbdb4fb410c9537cebea958896fb32cb1ec39eef
Adam Demasi <adam@hbang.com.au>
2017-06-16 23:43:18 +0930

    [postinst] try and see if we can get the damn plugins to load

--- postinst/BridgingHeader.h
+++ postinst/BridgingHeader.h
@@ -1,6 +1,23 @@
 @import Foundation;
 @import MobileCoreServices;
 
+//#import <Foundation/NSExtension.h>
+
+typedef void (^NSExtensionMatchingCompletion)(NSArray *result);
+
+@interface PKDiscoveryDriver : NSObject @end
+
+@class PKDiscoveryDriver;
+
+@interface NSExtension : NSObject
+
++ (PKDiscoveryDriver *)beginMatchingExtensionsWithAttributes:(NSDictionary *)attributes completion:(NSExtensionMatchingCompletion)completion;
++ (void)endMatchingExtensions:(PKDiscoveryDriver *)discoverer;
+
+@property (nonatomic, retain) NSString *identifier;
+
+@end
+
 #import <MobileCoreServices/LSApplicationProxy.h>
 #import <MobileCoreServices/LSApplicationWorkspace.h>
 #import <MobileCoreServices/LSPlugInKitProxy.h>
--- postinst/postinst.swift
+++ postinst/postinst.swift
@@ -2,6 +2,8 @@ import MobileCoreServices
 
 let workspace = LSApplicationWorkspace.default()!
 
+// MARK: - Helpers
+
 func registerApp(identifier: String, path: URL) {
 	// try to get an app with a matching name
 	let app = LSApplicationProxy(forIdentifier: identifier)
@@ -19,7 +21,7 @@ func registerApp(identifier: String, path: URL) {
 	}
 }
 
-func registerPlugin(identifier: String, path: URL) {
+func registerPlugIn(identifier: String, path: URL) {
 	// get all plugins matching ours – which, for whatever reason, returns all plugins if it can’t
 	// find a match…
 	let plugins = workspace.plugins(withIdentifiers: [ identifier ], protocols: nil, version: nil) as! [LSPlugInKitProxy]
@@ -40,9 +42,43 @@ func registerPlugin(identifier: String, path: URL) {
 	}
 }
 
+func confirmPlugIns() -> DispatchTimeoutResult {
+	// check to confirm PlugInKit is aware of our extensions
+	let semaphore = DispatchSemaphore(value: 0)
+	var discoverer: PKDiscoveryDriver! = nil
+
+	discoverer = NSExtension.beginMatchingExtensions(attributes: [:], completion: { (result) -> Void in
+		// get our results, hopefully
+		if let actualResult = result as? [NSExtension] {
+			// filter to ones that are hopefully ours
+			let ours = actualResult.filter { $0.identifier.hasPrefix("ws.hbang.canzone.app.") }
+
+			// if there are two, we’re good
+			if ours.count == 2 {
+				// swift pls
+				NSExtension.end(matchingExtensions: discoverer)
+
+				// tell the semaphore
+				semaphore.signal()
+			}
+		}
+	})
+
+	// return the semaphore.wait() result – success or timedOut
+	return semaphore.wait(timeout: DispatchTime.now() + 4)
+}
+
+// MARK: - Main
+
 print("Registering components… This might take a moment.")
 
 // register the app, followed by its plugins. ew this could be better :/
 registerApp(identifier: "ws.hbang.canzone.app", path: URL(fileURLWithPath: "/Applications/Canzone.app"))
-registerPlugin(identifier: "ws.hbang.canzone.app.nowplayingwidget", path: URL(fileURLWithPath: "/Applications/Canzone.app/PlugIns/CanzoneNowPlayingWidget.appex"))
-registerPlugin(identifier: "ws.hbang.canzone.app.notificationcontent", path: URL(fileURLWithPath: "/Applications/Canzone.app/PlugIns/CanzoneNotificationContent.appex"))
+registerPlugIn(identifier: "ws.hbang.canzone.app.nowplayingwidget", path: URL(fileURLWithPath: "/Applications/Canzone.app/PlugIns/CanzoneNowPlayingWidget.appex"))
+registerPlugIn(identifier: "ws.hbang.canzone.app.notificationcontent", path: URL(fileURLWithPath: "/Applications/Canzone.app/PlugIns/CanzoneNotificationContent.appex"))
+
+// it should take only a fraction of a second if it works, so we assume a timeout means we never got
+// anything. by the way, have i mentioned how god damn amazing swifty GCD is??
+if confirmPlugIns() == .timedOut {
+	print("⚠️ Hmm, failed… Please try installing or updating an app from the App Store to activate the widget.")
+}
